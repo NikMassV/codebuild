c91cb950f0c88b4fe9f5e40bed5755e47ae41913
Mikita_Maistrenka <mmaistrenka@copyright.com>
2022-09-11 23:13:24 +0300

    add main

---
+++ src/main/java/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        MessageUtil messageUtil = new MessageUtil("from main");
+        messageUtil.printMessage();
+        messageUtil.salutationMessage();
+    }
+}
